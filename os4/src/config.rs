f2c380394c2c8bd0b0ef2a619267047eb2df1304
Juguang Yan <49258901+coolyjg@users.noreply.github.com>
2022-12-01 04:51:37 +0000

    add os4 os5

--- os4/src/config.rs
+++ os4/src/config.rs
@@ -5,9 +5,14 @@ pub const MEMORY_END: usize = 0x88000000;
 pub const PAGE_SIZE: usize = 0x1000;
 pub const PAGE_SIZE_BITS: usize = 0xc;
 pub const MAX_SYSCALL_NUM: usize = 500;
-// pub const BIG_STRIDE: usize = usize::MAX;
-pub const BIG_STRIDE: usize = 1 << 32;
 
 pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
 pub const TRAP_CONTEXT: usize = TRAMPOLINE - PAGE_SIZE;
+/// Return (bottom, top) of a kernel stack in kernel space.
+pub fn kernel_stack_position(app_id: usize) -> (usize, usize) {
+    let top = TRAMPOLINE - app_id * (KERNEL_STACK_SIZE + PAGE_SIZE);
+    let bottom = top - KERNEL_STACK_SIZE;
+    (bottom, top)
+}
+
 pub const CLOCK_FREQ: usize = 12500000;
